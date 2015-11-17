4ce06df8b0f6e7768cd9b52e3073432460ecd608
McGiver- <gkolampas@gmail.com>
2015-11-16 20:55:01 -0500

    HackPrinceton  Project

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System.Drawing;
+namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Perpetua", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(21, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(367, 155);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Begin Processing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Font = new System.Drawing.Font("Perpetua", 20F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(1265, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 99);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Register";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Location = new System.Drawing.Point(417, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 482);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1474, 239);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(2, 33);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "jpg Image|*.jpg";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold);
+            this.richTextBox1.Location = new System.Drawing.Point(21, 228);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(367, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "Number of Males Customers :  Number of Female Customers : Average Customer Age :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Perpetua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1265, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 44);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Phone Number";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(900, 628);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(607, 124);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1914, 908);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MM SOLUTIONS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void pictureBox1_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video; 
+using AForge.Video.DirectShow;
+using System.Windows;
+using Newtonsoft.Json;
+using Twilio;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.IO;
+using System.Threading;
+using AForge;
+using AForge.Video;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Analytics;
+using System.IO;
+using System.Configuration;
+
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://facemobile.azure-mobile.net/", "xzMhfBEvqoGAJiUGUqhGhsigjgiwoS48");
+        public IMobileServiceTable<facetable> faceTable = MobileService.GetTable<facetable>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+        private Bitmap bit;
+        private int i;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in webcam)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+
+            //cam start
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+  
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            string AccountSid = "ACd489d0930dc658a3384b1b52a28cbced";
+            string AuthToken = "b4f632beb8bbf85f696693d0df69dba3";
+            FaceServiceClient faceClient = new FaceServiceClient("0e58dbc56e5445ac8fcdfa9ffbf5ef60");
+            if (!cam.IsRunning)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            bit.Save(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg");
+            Thread.Sleep(1000);
+            StorageCredentials storageCredentials = new StorageCredentials("faceimage", "DYrgou0cTTp6J7KDdMVVxR3BDtM31zh393oyf0CfWdTuihRUgDwyryQuIqj203SnPHMJVK7VvLGm/KtfIpUncw==");
+
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("facecontainer");
+            container.CreateIfNotExistsAsync();
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("pic.jpg");
+
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+            double[] ages = await UploadAndDetectFaceAges(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+            string[] genders = await UploadAndDetectFaceGender(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+            Guid[] ids = await UploadAndDetectFaceId(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+
+            InsertData(ids[0].ToString(), genders[0], ages[0].ToString(), textBox1.Text);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        public async Task<double[]> UploadAndDetectFaceAges(string imageFilePath, FaceServiceClient faceServiceClient)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream, false, true, true, false);
+
+                    var faceAge = faces.Select(face => face.Attributes.Age);
+
+                    return faceAge.ToArray();
+                }
+            }
+            catch (Exception e)
+            {
+                return new double[1];
+            }
+        }
+
+        public async Task<string[]> UploadAndDetectFaceGender(string imageFilePath, FaceServiceClient faceServiceClient)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream, false, true, true, false);
+
+                    var faceGender = faces.Select(face => face.Attributes.Gender);
+
+                    return faceGender.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return new string[1];
+            }
+        }
+
+        public async void InsertData(string idface, string gender, string age, string phonenumber)
+        {
+            var newFace = new facetable
+            {
+                IdFace = idface,
+                Gender = gender,
+                Age = age,
+                PhoneNumber = phonenumber,
+                Active = "Active"
+            };
+
+            await faceTable.InsertAsync(newFace);
+        }
+
+        public async Task<string> GetGenderFace(string idface)
+        {
+            var face = await faceTable.LookupAsync(idface);
+            return face.Gender;
+        }
+        public async Task<string> GetActiveFace(string idface)
+        {
+            var face = await faceTable.LookupAsync(idface);
+            return face.Active;
+        }
+
+        public async Task<string> GetAgeFace(string idface)
+        {
+            var face = await faceTable.LookupAsync(idface);
+            return face.Age;
+        }
+        public async Task<string> GetPhoneNumberFace(string idface)
+        {
+            var face = await faceTable.LookupAsync(idface);
+            return face.PhoneNumber;
+        }
+
+        public async Task<string> GetIdFace(string idface)
+        {
+            var face = await faceTable.LookupAsync(idface);
+            return face.IdFace;
+        }
+        public async Task<List<facetable>> FetchAllFaces()
+        {
+            var allCustomers = new List<facetable>();
+
+            var list = await faceTable.Take(50).Where(facetable => facetable.Active == "Active").ToListAsync();
+            foreach (var customer in list)
+            {
+                allCustomers.Add(customer);
+            }
+           
+
+            return allCustomers;
+        }
+
+        public async Task<Guid[]> UploadAndDetectFaceId(string imageFilePath, FaceServiceClient faceServiceClient)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream, false, true, true, false);
+
+                    var faceId = faces.Select(face => face.FaceId);
+
+                    return faceId.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+                return new Guid[1];
+            }
+        }
+        
+        async private void button2_Click(object sender, EventArgs e)
+        {
+            string AccountSid = "ACd489d0930dc658a3384b1b52a28cbced";
+            string AuthToken = "b4f632beb8bbf85f696693d0df69dba3";
+            FaceServiceClient faceClient = new FaceServiceClient("0e58dbc56e5445ac8fcdfa9ffbf5ef60");
+            double[] ages = await UploadAndDetectFaceAges(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+            string[] genders = await UploadAndDetectFaceGender(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+
+            while(true)
+            {
+                VerifyResult verification = new VerifyResult();
+                verification.Confidence = 0;
+                int numBoys = 0;
+                int numgirls = 0;
+                int ppl = 0;
+                int avgAge = 0;
+                int totAge = 0;
+                if (!cam.IsRunning)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+                bit.Save(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg");
+                Thread.Sleep(1000);
+
+                double[] ages1 = await UploadAndDetectFaceAges(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+                string[] genders2 = await UploadAndDetectFaceGender(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+
+                StorageCredentials storageCredentials = new StorageCredentials("faceimage", "DYrgou0cTTp6J7KDdMVVxR3BDtM31zh393oyf0CfWdTuihRUgDwyryQuIqj203SnPHMJVK7VvLGm/KtfIpUncw==");
+
+                CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer container = blobClient.GetContainerReference("facecontainer");
+                container.CreateIfNotExistsAsync();
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference("pic.jpg");
+
+                using (var fileStream = System.IO.File.OpenRead(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg"))
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+                Guid[] ids = await UploadAndDetectFaceId(@"C:\Users\ma_eg_000\Desktop\PrincetonHack\pic.jpg", faceClient);
+
+                InsertData(ids[0].ToString(), genders2[0], ages1[0].ToString(), "5149941737");
+                
+                List<facetable> ftable = await FetchAllFaces();
+                string toCall = "null";
+                foreach(facetable fTable in ftable)
+                {
+                    ppl++;
+                    if(fTable.Gender == "male")
+                    {
+                        numBoys++;
+                    }
+                    else
+                    {
+                        numgirls++;
+                    }
+                    totAge = totAge + Int32.Parse(fTable.Age);
+                    Guid id2 = new Guid(fTable.IdFace);
+                    VerifyResult temp = await faceClient.VerifyAsync(ids[0],id2 );
+                    if (temp.Confidence >= verification.Confidence)
+                    {
+                        verification = temp;
+                        toCall = fTable.PhoneNumber;
+                    }
+                }
+                avgAge = totAge / ppl;
+                if(verification.Confidence>= 0.40)
+                {
+                    richTextBox1.Text = "Number of Males Customers : "+numBoys+ " Number of Female Customers :"+numgirls+ " Average age of Customers :"+avgAge;
+                    var twilio = new TwilioRestClient(AccountSid, AuthToken);
+                    var message = twilio.SendMessage("16263449948", toCall, "WE HAVE THE BEST DEALS FOR YOU TODAY!!! Free Selfie Sticks and T-Shirts Gallore", "");
+                }
+                Thread.Sleep(1000);
+                
+            }
+        }
+    }
+
+    public class facetable
+    {
+        public string id { get; set; }
+        public string IdFace { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Active { get; set; }
+    }
+}
